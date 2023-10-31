c23172f43427c422fc30a32b7ed95214ca84b5a5
parin-prog <parinvaghani001@gmail.com>
2023-10-31 17:19:31 +0530

    Initial Commit

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,21 @@
+import { Navigate, Outlet } from 'react-router-dom';
+
+const AuthLayout = () => {
+  let authenticated = false;
+  return (
+    <>
+      {authenticated ? (<Navigate to="/" />
+      ) : (
+        <div className='flex'>
+          <section className='flex flex-1 justify-center items-center flex-col py-10'>
+            <Outlet />
+          </section>
+          <img src='/assets/images/side-img.svg'
+            alt='logo'
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat" />
+        </div>)}
+    </>
+  )
+}
+
+export default AuthLayout
---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+
+const SignupForm = () => {
+  return (
+    <>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default SignupForm
